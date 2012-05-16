5c5a3ac5ff78d27008e5d8d41b66a87ae72267ee
Brian Link <cpsubrian@gmail.com>
2012-05-15 17:25:33 -0700

    Changes a comment in the service scaffolding.

--- scaffolds/service/lib/plugin.tpl.js
+++ scaffolds/service/lib/plugin.tpl.js
@@ -8,12 +8,12 @@ require('pkginfo')(module);
 // The Service is being attached to an application.
 // `this` will be a reference to the application.
 // You might push models, templates, etc. onto the app here.
+// Services can bind routes via this.router (a director router).
 exports.attach = function(options) {
 
 }
 
 // The Service is being detached from the application.
-// Services can bind routes via this.router (a director router).
 exports.detach = function() {
 
 }
