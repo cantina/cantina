4fc730369610bd072f18e1b2f62fcc22ba70bcb2
Brian Link <cpsubrian@gmail.com>
2012-06-12 15:32:20 -0700

    Moves utils and their tests to cantina-utils module.

--- test/plugin-utils.js
+++ test/plugin-utils.js
@@ -7,28 +7,7 @@ var assert = require('assert'),
 
 describe('Plugin: Utils', function() {
   var app = cantina.createApp();
-
-  describe('Defaults', function() {
-    var start, defaults, end;
-
-    start = {
-      color: 'red',
-      count: 5
-    };
-    defaults = {
-      count: 10,
-      size: 'large'
-    };
-    end = {
-      color: 'red',
-      count: 5,
-      size: 'large'
-    };
-
-    it('should apply default properties to an object', function() {
-      app.utils.defaults(start, defaults);
-      assert.deepEqual(start, end, 'Default properties were copied incorrectly');
-    });
-
+  it ('should expose utils on the app object', function() {
+    assert.equal(typeof app.utils, 'object', 'The utils are not exposed');
   });
 });
