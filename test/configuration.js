deb1a5c83ee26cb2456d50f0845316b0abd781e2
Brian Link <cpsubrian@gmail.com>
2012-08-20 16:03:45 -0700

    Adds app.configure()

---
+++ test/configuration.js
@@ -0,0 +1,50 @@
+/**
+ * Tests plugin configuration.
+ */
+
+var Cantina = require('../').Cantina,
+    assert = require('assert');
+
+describe('configuration', function() {
+  var app;
+  var plugin = {
+    name: 'prefixer',
+    provides: ['prefix'],
+    init: function(conf, imports, register) {
+      if (!conf.prefix) {
+        return register(new Error('No prefix in config'));
+      }
+      register(null, {
+        prefix: function(str) {
+          return conf.prefix + str;
+        }
+      });
+    }
+  };
+
+  beforeEach(function() {
+    app = new Cantina();
+  });
+
+  it('uses per-plugin configuration passed into `app.use()`', function(done) {
+    app.use(plugin, { prefix: 'hello:' });
+    app.init();
+    app.on('ready', function(app) {
+      assert.equal(app.services.prefix('world'), 'hello:world', 'The prefix was not added correctly');
+      done();
+    });
+  });
+
+  it('uses configuration specified with `app.conf()`', function(done) {
+    app.use(plugin);
+    app.configure({
+      prefixer: { prefix: 'hi ' }
+    });
+    app.init();
+    app.on('ready', function(app) {
+      assert.equal(app.services.prefix('world'), 'hi world', 'The prefix was not added correctly');
+      done();
+    });
+  });
+
+});
--- test/sorting.js
+++ test/sorting.js
@@ -8,14 +8,22 @@ var Cantina = require('../').Cantina,
 describe('plugin sorting', function() {
   var app;
 
+  // Adds an empty init method to test plugin.
+  function plugin(props) {
+    props.init = function(conf, imports, register) {
+      register(null, {});
+    };
+    return props;
+  }
+
   before(function() {
     app = new Cantina();
-    app.use({ name: 'alpha' });
-    app.use({ name: 'beta', consumes: ['a'] });
-    app.use({ name: 'gamma', consumes: ['b'] });
-    app.use({ name: 'delta', consumes: ['a', 'c'] });
-    app.use({ name: 'epsilon', consumes: ['b'], provides: ['a'] });
-    app.use({ name: 'zeta', provides: ['b', 'c'] });
+    app.use(plugin({ name: 'alpha' }));
+    app.use(plugin({ name: 'beta', consumes: ['a'] }));
+    app.use(plugin({ name: 'gamma', consumes: ['b'] }));
+    app.use(plugin({ name: 'delta', consumes: ['a', 'c'] }));
+    app.use(plugin({ name: 'epsilon', consumes: ['b'], provides: ['a'] }));
+    app.use(plugin({ name: 'zeta', provides: ['b', 'c'] }));
   });
 
   it('correctly sorts the plugins', function() {
@@ -37,7 +45,7 @@ describe('plugin sorting', function() {
   });
 
   it('throws when plugin dependencies cannot be resolved', function() {
-    app.use({ name: 'eta', consumes: ['d'] });
+    app.use(plugin({ name: 'eta', consumes: ['d'] }));
     assert.throws(function() {
       app.sortedPlugins();
     }, /Could not resolve dependencies/, 'Sorting did not throw the resolve error');
@@ -45,10 +53,10 @@ describe('plugin sorting', function() {
 
   it('keeps plugins in order that don\'t need to be rearranged', function() {
     app.remove('eta');
-    app.use({ name: 'first', consumes: ['a', 'b'] });
-    app.use({ name: 'second', consumes: ['a', 'b'] });
-    app.use({ name: 'third', consumes: ['a', 'b'] });
-    app.use({ name: 'fourth', consumes: ['a', 'b'] });
+    app.use(plugin({ name: 'first', consumes: ['a', 'b'] }));
+    app.use(plugin({ name: 'second', consumes: ['a', 'b'] }));
+    app.use(plugin({ name: 'third', consumes: ['a', 'b'] }));
+    app.use(plugin({ name: 'fourth', consumes: ['a', 'b'] }));
 
     var names = app.sortedPlugins().map(function(plugin) {
       return plugin.name;
