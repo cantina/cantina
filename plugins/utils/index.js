1e54eff6217d33e7c66035e165fb93cfe6d36680
Brian Link <cpsubrian@gmail.com>
2012-08-31 19:06:56 -0400

    Adds utils plugin.

---
+++ plugins/utils/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  name: 'utils',
+  version: require('../../package.json').version,
+
+  init: function(app, done) {
+    app.utils = require('util');
+    app.utils.async = require('async');
+    app.utils.glob = require('glob');
+
+    app.defaults = function (obj, defaults) {
+      Object.keys(defaults).forEach(function(key) {
+        if (!obj.hasOwnProperty(key)) {
+          obj[key] = copy[key];
+        }
+      });
+    };
+
+    done();
+  }
+};
