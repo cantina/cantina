ac404bdce29e46bccda57a5e15ce6e0eb8da11f1
Carlos Rodriguez <carlos@s8f.org>
2012-09-06 14:10:39 -0700

    add app.utils.clone()

--- plugins/utils/index.js
+++ plugins/utils/index.js
@@ -8,10 +8,23 @@ module.exports = {
     app.utils.async = require('async');
     app.utils.glob = require('glob');
 
+    // Single-level-deep clone
+    app.utils.clone = function (obj) {
+      if (typeof obj !== 'object') {
+        return obj;
+      }
+      var ret = {};
+      Object.keys(obj).forEach(function (k) {
+        ret[k] = obj[k];
+      });
+      return ret;
+    };
+
+    // Single-level-deep defaults
     app.utils.defaults = function (obj, defaults) {
       Object.keys(defaults).forEach(function(k) {
         if (typeof obj[k] === 'undefined') {
-          obj[k] = defaults[k];
+          obj[k] = app.utils.clone(defaults[k]);
         }
       });
     };
