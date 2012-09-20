60c4e7671678b809d5c5f2e34694adf18f1bc151
Brian Link <cpsubrian@gmail.com>
2012-09-20 11:36:14 -0700

    Updates examples/static for 2.x

--- examples/static/README.md
+++ examples/static/README.md
@@ -3,5 +3,4 @@ Cantina Example - Static
 
 Very basic static file server using Cantina.
 
-This example is a little too trivial to be useful in and of itself, however, it
-probably serves as the basis for 90% of cantina web apps.
+This tiny example app probably serves as the basis for 90% of cantina web apps.
--- examples/static/index.js
+++ examples/static/index.js
@@ -1,9 +1,16 @@
-var cantina = require('cantina');
-var plugins = ['cantina-http', 'cantina-middler', 'cantina-buffet'];
+var app = require('../../');
 
-cantina.createApp(plugins, function(err, app) {
+// Setup the app. Loads configuration, locates the application root, and loads
+// default core plugins (utils).
+app.setup(function(err) {
   if (err) return console.log(err);
 
-  // You could do stuff here if you want, but it would be better to put
-  // functionality in plugins.
+  // Load plugins.
+  require(app.plugins.http);
+  require(app.plugins.middleware);
+  require(app.plugins.static);
+
+  // Initialize the app. Runs all plugin 'init' listeners, then runs all
+  // 'ready' listeners.
+  app.init();
 });
