a4509969552efca8f350f18d0c3afb9f5bfebbe8
Brian Link <cpsubrian@gmail.com>
2013-07-16 17:29:44 -0700

    Static middleware doesn't need to be in 'init'.

--- plugins/static/index.js
+++ plugins/static/index.js
@@ -9,22 +9,20 @@ app.conf.add({
   }
 });
 
-app.on('init', function () {
-  var conf = app.conf.get('static');
+var conf = app.conf.get('static');
 
-  // Resolve root to the app root.
-  var rootPath = path.resolve(app.root, conf.path);
+// Resolve root to the app root.
+var rootPath = path.resolve(app.root, conf.path);
 
-  // Create buffet middleware.
-  var buffetMiddleware = buffet(rootPath, conf);
+// Create buffet middleware.
+var buffetMiddleware = buffet(rootPath, conf);
 
-  // Add buffet to middler stack.
-  app.middleware.add(buffetMiddleware);
+// Add buffet to middler stack.
+app.middleware.add(buffetMiddleware);
 
-  // Optionally add buffet notFound handler.
-  if (conf.notFound) {
-    app.middleware.last(buffetMiddleware.notFound);
-  }
+// Optionally add buffet notFound handler.
+if (conf.notFound) {
+  app.middleware.last(buffetMiddleware.notFound);
+}
 
-  app.buffet = buffet;
-});
+app.buffet = buffet;
